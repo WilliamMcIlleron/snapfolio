47bd762de422a161a4ca11948db4948d3c69c2a2
William McIlleron <williamjonahmci@icloud.com>
2025-07-04 13:00:07 +0200

    Added initial code

---
+++ script.js
@@ -0,0 +1,20 @@
+
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      speed: 800,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      centeredSlides: true,
+      breakpoints: {
+        768: {
+          slidesPerView: 1.2,
+        },
+        1024: {
+          slidesPerView: 1.5,
+        },
+      },
+    });
